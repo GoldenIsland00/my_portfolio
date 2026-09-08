b005de0221f96922b4713b00cfabe2d417356f5b
Sirgae youfski <sirgae.youfski@gmail.com>
2026-09-08 23:03:33 +0330

    Update settings.py

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-change-me-in-p
 
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True').lower() in ('1', 'true', 'yes')
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'https://villiam-taghizadeh.onrender.com/,villiam-taghizadeh.onrender.com').split(',')
 
 INSTALLED_APPS = [
     'modeltranslation',
